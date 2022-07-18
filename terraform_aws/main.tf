1207bd4b319e0f1531c0f7cc50a9074ca5411e24
rsambhi <rajinder.sambhi@gmail.com>
2022-07-18 18:54:08 +0000

    Updated the file

--- terraform_aws/main.tf
+++ terraform_aws/main.tf
@@ -1,26 +1,3 @@
-################################
-# VARIABLES
-################################
-variable "aws_access_key" {}
-
-variable "aws_secret_key" {}
-
-variable "ssh_key_name" {}
-
-variable "private_key_path" {}
-
-variable "region" {
-  default = "us-east-2"
-}
-
-variable "vpc_cidr" {
-  default = "172.16.0.0/16"
-}
-
-variable "subnet1_cidr" {
-  default = "172.16.0.0/24"
-}
-
 terraform {
   required_providers {
     aws = {
